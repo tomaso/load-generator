da773bc9fc4808c57ab857f183c8f08068405fe0
Tomáš Kouba <tomaso.kouba@gmail.com>
2025-02-05 23:00:13 +0100

    Add go project files and TODO list

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tomaso/load-generator
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+ TODO:
+ - Define and document configuration file
+ - Read configuration from a file using viper
+ - Find a module/lib for simple option/argument parsing
+ - Implement cpu load
+*/
+
+func main() {
+
+}
