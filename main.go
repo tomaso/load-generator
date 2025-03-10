25b5a7134fa5cee4c029b8ed84007ee2ded28948
Tomáš Kouba <tomas_kouba@rapid7.com>
2025-03-10 17:02:58 +0100

    Add basic option parsing

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"fmt"
+)
+
 /*
  TODO:
  - Define and document configuration file
@@ -8,6 +14,19 @@ package main
  - Implement cpu load
 */
 
+var ErrHelp = errors.New("flag: help requested")
+
 func main() {
 
+	help := flag.Bool("help", false, "Show this help.")
+	cpuload := flag.Int("cpuload", 20, "Basic cpu load per core that will be generated.")
+	threads := flag.Int("threads", 1, "Number of threads to run with the specified load.")
+	mem := flag.Int("mem", 100, "Memory to allocate (in MiB).")
+
+	flag.Parse()
+	if *help {
+		flag.PrintDefaults()
+		return
+	}
+	fmt.Printf("cpuload: %d, threads: %d, memory: %d\n", *cpuload, *threads, *mem)
 }
